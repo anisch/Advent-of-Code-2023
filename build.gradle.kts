e76e05f74004af5d15fb629029ffea6a3212a535
anisch <8927916+anisch@users.noreply.github.com>
2023-11-13 15:17:09 +0100

    add simple day generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,3 +17,47 @@ tasks {
         gradleVersion = "8.4"
     }
 }
+
+abstract class DayGeneratorTask : DefaultTask() {
+    @get:Input
+    @get:Option(option = "day", description = "set the day")
+    abstract val day: Property<Int>
+
+    @get:OutputDirectory
+    abstract val outputDir: DirectoryProperty
+
+    @TaskAction
+    fun createFile() {
+        val txtDay = String.format("%02d", day.get())
+        val content =
+            """
+                fun main() {
+                    fun part1(input: List<String>): Int {
+                        return input.size
+                    }
+
+                    fun part2(input: List<String>): Int {
+                        return input.size
+                    }
+
+                    // test if implementation meets criteria from the description, like:
+                    val testInput = readInput("Day${txtDay}_test")
+                    check(part1(testInput) == 1)
+
+                    val input = readInput("Day$txtDay")
+                    part1(input).println()
+                    part2(input).println()
+                }
+
+            """.trimIndent()
+
+        val targetFile = outputDir.file("Day$txtDay.kt").get().asFile
+        targetFile.writeText(content)
+    }
+}
+
+// Customize the greeting
+tasks.register<DayGeneratorTask>("createDay") {
+    day = 1
+    outputDir = layout.projectDirectory.dir("src")
+}
