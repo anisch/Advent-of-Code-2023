593b3996ba904b6cefd4845143c86aadd11a9063
anisch <8927916+anisch@users.noreply.github.com>
2023-11-13 15:15:33 +0100

    add some helpers

--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,23 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+/**
+ * Swap elements inside MutableList
+ */
+fun MutableList<Any>.swap(a: Int, b: Int) {
+    this[a] = this[b].also { this[b] = this[a] }
+}
+
+/**
+ * Vector
+ */
+data class Vec2D(val x: Int, val y: Int) {
+    operator fun plus(o: Vec2D) = Vec2D(x + o.x, y + o.y)
+    operator fun minus(o: Vec2D) = Vec2D(x - o.x, y - o.y)
+}
+
+data class Vec3D(val x: Int, val y: Int, val z: Int) {
+    operator fun plus(o: Vec3D) = Vec3D(x + o.x, y + o.y, z + o.z)
+    operator fun minus(o: Vec3D) = Vec3D(x - o.x, y - o.y, z - o.z)
+}
